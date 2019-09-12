4da44b3303f0058ad9b46c90ea666eae2709a66b
Sugizaki Yukimasa <ysugi@idein.jp>
2019-09-12 17:20:42 +0900

    Add vc6qpudisas.c

---
+++ vc6qpudisas.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+/* From Mesa */
+#include <qpu_disasm.h>
+#include <ralloc.h>
+
+int main(void)
+{
+    struct v3d_device_info devinfo = {
+        .ver = 42,
+    };
+
+    char line[4096];
+
+    while (fgets(line, sizeof(line), stdin) != NULL) {
+        const unsigned long long insn = strtoull(line, NULL, 0);
+
+        const char *disasm = v3d_qpu_disasm(&devinfo, insn);
+
+        puts(disasm);
+
+        ralloc_free((void*) disasm);
+    }
+
+    return 0;
+}
